1ce11de5f36e7c370ac225969b00a009b23e17d0
Matthias Mazenauer <matthias.mazenauer@statistik.ji.zh.ch>
2017-10-09 10:53:29 +0200

    thin Layout with Map

--- js/script_small.js
+++ js/script_small.js
@@ -6,7 +6,7 @@
 		// Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
 		var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
 		var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
-		// At least Safari 3: "[object HTMLElementConstructor]"
+		// At least Safari 3+: "[object HTMLElementConstructor]"
 		var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
 		var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
 
@@ -32,7 +32,7 @@
 		///////////////////////
 		// DEFS
 		//
-		var margin = {top: 20, right: 20, bottom: 20, left: 20},
+		var margin = {top: 20, right: 20, bottom: 25, left: 20},
 			gRatio = (1+Math.sqrt(5))/2,
 			ch1903 = {'x1':669625, 'x2':716125, 'y1':224725, 'y2': 283325},
 			pi = Math.PI,
@@ -40,11 +40,11 @@
 			legendeTop = 0,
 			//w = 532,
 			w = 570 - margin.left - margin.right,
-			h = 550 - margin.top - margin.bottom,
-			wPyr = 370,
+			h = 555 - margin.top - margin.bottom,
+			wPyr = 360,
 			hPyr=410,
 			hPyrPer = hPyr/111,
-			scaleF = 0.3,
+			scaleF = 0.37,
 			renderAusl = 1,
 			spiegel = 1;
 
@@ -56,13 +56,26 @@
 				.attr('id', 'svg')
 				.attr('width', w + margin.left + margin.right)
 				.attr('height', h + margin.top + margin.bottom)
-			.append('g')
+				.attr('aria-labelledby','title')
+				.attr('ariadescribedby', 'desc')
+				.attr('role', 'img');
+		// svg.append('title')
+		// 	.text('Alterspyramide des Kantons Zürich')
+		// 	.attr('id', 'title')
+		// 	.attr('pointer-events', 'none');
+		svg.append('desc')
+				.attr('desc', 'Eine interaktive Grafik zu den Untersciedlichen Alterspyramiden der Regionen des Kantons Zürich.')
+				.attr('id', 'desc');
+
+		var svgGr = svg.append('g')
 				.attr("transform", "translate(" + (margin.left) + "," + margin.top + ")");
 
-    var pyrGrBack = d3.select('#svg').append('g').attr('id','pyrGrBack').attr('transform', 'translate(' + (40) + ',' + (0) + ')');
-    var pyrGr = d3.select('#svg').append('g').attr('id','pyrGr').attr('transform', 'translate(' + (40+wPyr/2) + ',' + (0) + ')');
 
-		var mapPfade = svg.append('g').attr('id', 'mapPfade').attr('transform', 'translate(' + (wPyr) + ',' + (40) + ')scale('+scaleF+')');
+
+    var pyrGrBack = d3.select('#svg').append('g').attr('id','pyrGrBack').attr('transform', 'translate(' + (30) + ',' + (50) + ')');
+    var pyrGr = d3.select('#svg').append('g').attr('id','pyrGr').attr('transform', 'translate(' + (30+wPyr/2) + ',' + (50) + ')');
+
+		var mapPfade = d3.select('#svg').append('g').attr('id', 'mapPfade').attr('transform', 'translate(' + (wPyr+15) + ',' + (40) + ')scale('+scaleF+')');
 		var karte = mapPfade.append('g').attr("id", "chart").append("g").attr('id', 'karte');
 
     var defs = d3.select('#svg').append("defs")
@@ -80,9 +93,9 @@
 	        .attr('transform',"translate(0,0)")
 	        .attr('fill',"grey" );
 		
-		var slider = svg.append('g')
+		var slider = svgGr.append('g')
 			.attr('class', 'slider')
-			.attr('transform', 'translate('+(w-wPyr)+','+(h)+')');
+			.attr('transform', 'translate('+(30)+','+(h)+')');
 	
 		var clip = defs.append('clipPath')
 			.attr('id', 'edge')
@@ -127,7 +140,7 @@
 		//
 		// Data
 		//
-		var file1 = 'aba_reshape.csv',
+		var file1 = 'STAT_ZH2017_bevoelkerung_regionen_prognose.csv',
 			file2 = 'RegionenZH_Topo.json';
 
 		d3_queue.queue()
@@ -137,12 +150,12 @@
 					gemeinde: d.region,
 					jahr: parseYear(d.JAHR),
 					alter: +d.alter,
-					fCH: +d["1_2"],
-					mCH: +d["1_1"],
-					fAusl: +d["2_2"],
-					mAusl: +d["2_1"],
-					mTot: +d["1_1"]+(+d["2_1"]),
-					fTot: +d["1_2"]+(+d["2_2"])
+					fCH: +d.fCH,
+					mCH: +d.mCH,
+					fAusl: +d.fAusl,
+					mAusl: +d.mAusl,
+					mTot: +d.mCH+(+d.mAusl),
+					fTot: +d.fCH+(+d.fAusl)
 				}
 			})
 			.defer(d3.json,'data/'+file2)
@@ -177,23 +190,22 @@
 			years = [],
 			handle;
 
-		svg.append('text')
+		svgGr.append('text')
 			.attr('id', 'loader')
 			.attr('x', 185)
 			.attr('y', h/2)
 			.attr('text-anchor', 'middle')
 			.text('Daten werden geladen')
+			.style('font-family', 'Helvetica');
 
 		var yScalePyr = d3.scaleLinear()
 			.domain([0,110])
-			.range([h-60,30])
+			.range([hPyr,0])
       
     var yAxis = d3.axisLeft()
       .scale(yScalePyr)
       .tickSize(wPyr+5)
-      .tickValues([0,15,65,100])
-      //.ticks(3)
-      //.tickFormat(d3.format('.0%'))
+      .tickValues([0,20,65,100])
       ;
 
     var yAxisGroup = d3.select('#pyrGrBack').append('g')
@@ -217,9 +229,24 @@
 		var colorScale;
 
 
-		d3.select('#controlUL').append('li')
-		  .attr('id', 'ausl')
-		  .text('nur Schweizer/innen');
+		var button1 = d3.select('#controlUL').append('li')
+		  .attr('id', 'ausl');
+		button1.text('nur Schweizer/innen');
+		
+		var button2 = d3.select('#controlUL').append('li')
+		  .attr('id', 'spiegel');
+		button2
+		  .html('Männer und Frauen überlagern');
+
+
+		var export1 = d3.select('#exportUL').append('li')
+		  .attr('id', 'expBild');
+		export1.text('Bild speichern');
+		
+		var export2 = d3.select('#exportUL').append('li')
+		  .attr('id', 'expData');
+		export2
+		  .html('Daten speichern');
 
 	  d3.select('#ausl')
 	    .attr('cursor', 'pointer')
@@ -237,9 +264,6 @@
 	      pyramide(dataSel,jahrSel,renderAusl,spiegel);
 	    });
 
-		d3.select('#controlUL').append('li')
-		  .attr('id', 'spiegel')
-		  .text('Männer spiegeln');
 
 	  d3.select('#spiegel')
 	    .attr('cursor', 'pointer')
@@ -275,7 +299,7 @@
 					return {
 						"totJahr": d3.sum(leaves, function(d) {return d.fCH+d.mCH+d.fAusl+d.mAusl;}),
 						"arbJahr": d3.sum(leaves, function(d) {
-							if(d.alter>=15&&d.alter<65) {
+							if(d.alter>=20&&d.alter<65) {
 								return d.fCH+d.mCH+d.fAusl+d.mAusl;
 							} else{
 								return 0;
@@ -325,56 +349,50 @@
         .range(['#dce8ef', '#0076BD'])
         .interpolate(d3.interpolateLab);
 
-//	Legende für Karte
-//
-//
+			var anzRect = 30,
+				padding = 20
+				legW = w-wPyr-padding,
+				rectW = legW/anzRect;
+		    
+	    colorLegScale = d3.scaleLinear()
+	      .domain([0,anzRect-1])
+	      //.range(['#e2001a', '#0076BD'])
+	      .range([arbProzMinAll, arbProzMaxAll]);
+	  	
+	  	var legGr = pyrGrBack.append('g')
+	  		.attr('id', 'legGr')
+	  		.attr('transform', 'translate('+(wPyr+padding)+','+(320)+')');
+	    legGr.append('text')
+	    	.attr('x', -rectW-7)
+	    	.attr('y', -8)
+	    	.text('Anteil der 20- bis 64-Jährigen')
+					.style('font-family', 'Helvetica');
 
-		var anzRect = 30,
-			legW = w-wPyr-80,
-			rectW = legW/anzRect;
-	    
-    colorLegScale = d3.scaleLinear()
-      .domain([0,anzRect-1])
-      //.range(['#e2001a', '#0076BD'])
-      .range([arbProzMinAll, arbProzMaxAll]);
-  	
-  	var legGr = pyrGrBack.append('g')
-  		.attr('id', 'legGr')
-  		.attr('transform', 'translate('+(-(w-wPyr)+20)+','+(h-30)+')');
-    legGr.append('text')
-    	.attr('x', -rectW)
-    	.attr('y', -8)
-    	.text('Anteil der 15- bis 65-Jährigen');
+	    for(i=0;i<anzRect;i++) {
+	    	legGr.append('rect')
+	    		.attr('x', rectW*i-rectW/2)
+	    		.attr('y', 0)
+	    		.attr('width', rectW)
+	    		.attr('height', 15)
+	    		.style('stroke', 'none')
+	    		.style('fill', colorScale(colorLegScale(i)));
 
-    for(i=0;i<anzRect;i++) {
-    	legGr.append('rect')
-    		.attr('x', rectW*i-rectW/2)
-    		.attr('y', 0)
-    		.attr('width', rectW)
-    		.attr('height', 15)
-    		.style('stroke', 'none')
-    		.style('fill', colorScale(colorLegScale(i)));
-
-    	if(i%10==0||i==29) {
-    		legGr.append('text')
- 		   		.attr('x', rectW*i)
- 		   		.attr('y', 28)
- 		   		.style('text-anchor', 'middle')
- 		   		.text(Math.round(colorLegScale(i)*100)+'%');
-    	}
-    }
-
+	    	if(i%10==0||i==29) {
+	    		legGr.append('text')
+	 		   		.attr('x', rectW*i)
+	 		   		.attr('y', 28)
+	 		   		.style('text-anchor', 'middle')
+	 		   		.text(Math.round(colorLegScale(i)*100)+'%')
+						.style('font-family', 'Helvetica');
+	    	}
+	    }
 
       d3.select('#loader').remove();
-
-
 			
 			renderSlide();
 			renderPyramideBack();
 
-
 			var geom = mapData.objects.regionenZH_geo.geometries;
-
 			dataSel = topojson.feature(mapData, mapData.objects.regionenZH_geo).features[4];
 
 			//////////////
@@ -403,7 +421,7 @@
 					if (d.properties.ObWin == 0) {
 						return 1;
 					} else {
-						return 0.8;
+						return 1;
 					}
 				})
 				.style('fill', function(d) {
@@ -465,7 +483,8 @@
 			  .enter().append('text')
 			    .attr('x', slideScale)
 			    .attr('text-anchor', 'middle')
-			    .text(function(d) { return d; });
+			    .text(function(d) { return d; })
+				.style('font-family', 'Helvetica');
 
 			handle = slider.insert('circle', '.track-overlay')
 		    .attr('class', 'handle')
@@ -479,6 +498,8 @@
 				.attr('cx', slideScale(jahrS));
 
 			pyramide(dataSel,jahrSel,renderAusl,spiegel);
+			
+			//Jahreszahl beim Slider nicht über Rand hinaus:
 			var jx;
 			if(jahrS<=1998) {
 				jx = 1998;
@@ -515,46 +536,100 @@
 			pyrGrBack.append('text')
 				.attr('id', 'gemeindeText')
 				.attr('class', 'title')
-				.attr('x', -wPyr)
-				.attr('y', 45)
-				.style('font-size', '36px')
-				.text('Klicken Sie auf eine Gemeinde');
+				.attr('x', wPyr/2)
+				.attr('y', -10)
+				.style('font-size', '28px')
+				.text('Klicken Sie auf eine Gemeinde')
+				.style('font-family', 'Helvetica')
+				.style('text-anchor', 'middle');
 			
 			pyrGrBack.append('text')
 				.attr('id', 'jahrText')
 				.attr('class', 'title')
 				.style('text-anchor', 'middle')
 				.attr('x', 19+slideScale(formatYear(jahrSel)))
-				.attr('y', h)
-				.style('font-size', '36px')
-				.text(formatYear(jahrSel));
-			
+				.attr('y', hPyr+52)
+				.style('font-size', '28px')
+				.text(formatYear(jahrSel))
+				.style('font-family', 'Helvetica');
+	
+					
+			pyrGrBack.append('text')
+				.attr('x', -h+227)
+				.attr('y', -14)
+				.style('fill', 'dimgrey')
+				.style('opacity', 1)
+				.text('Altersklassen')
+				.attr('text-anchor', 'start')
+				.attr('transform', 'rotate(-90)')
+				.style('font-family', 'Helvetica');
+
+
 			pyrGrBack.append('text')
 				.attr('class', 'title')
 				.attr('x', 0)
-				.attr('y', 53)
+				.attr('y', 10)
 				.style('fill', '#407B9F')
-				.style('opacity', 0.7)
-				.text('Männer');    
- 			
+				.style('opacity', 1)
+				.text('Männer')
+				.style('font-family', 'Helvetica');    
+			
+			pyrGrBack.append('text')
+				.attr('class', 'title maennerCH')
+				.attr('x', 0)
+				.attr('y', 26)
+				.style('stroke', 'none')
+				.text('Schweiz')
+				.style('font-family', 'Helvetica');  
+			
+			pyrGrBack.append('text')
+				.attr('class', 'title')
+				.attr('x', 70)
+				.attr('y', 26)
+				.style('fill', '#3F98CC')
+				.style('opacity',1)
+				.text('Ausland')
+				.style('font-family', 'Helvetica');
+
 			pyrGrBack.append('text')
 				.attr('class', 'title')
 				.attr('x', wPyr)
-				.attr('y', 53)
+				.attr('y', 10)
 				.attr('text-anchor', 'end')
 				.style('fill', '#857091')
-				.style('opacity', 0.7)
-				.text('Frauen');         
-    
+				.style('opacity',1)
+				.text('Frauen')
+				.style('font-family', 'Helvetica');         
+   
+			pyrGrBack.append('text')
+				.attr('class', 'title')
+				.attr('x', wPyr)
+				.attr('y', 26)
+				.attr('text-anchor', 'end')
+				.style('fill', '#857091')
+				.style('opacity',1)
+				.text('Schweiz')
+				.style('font-family', 'Helvetica');    
+			
+			pyrGrBack.append('text')
+				.attr('class', 'title')
+				.attr('x', wPyr-70)
+				.attr('y', 26)
+				.attr('text-anchor', 'end')
+				.style('fill', '#A585B7')
+				.style('opacity',1)
+				.text('Ausland')
+				.style('font-family', 'Helvetica'); 
+
 		  var xAxisGroupF = d3.select('#pyrGrBack').append('g')
 				.attr('id', 'xAxisF')
 				.attr('class', 'gridL')
-				.attr('transform', 'translate('+(wPyr/2)+','+(32+10*hPyrPer)+')');
+				.attr('transform', 'translate('+(wPyr/2)+','+(10*hPyrPer)+')');
     
 		  var xAxisGroupM = d3.select('#pyrGrBack').append('g')
 				.attr('id', 'xAxisM')
 				.attr('class', 'gridL')
-				.attr('transform', 'translate('+(0)+','+(32+10*hPyrPer)+')');
+				.attr('transform', 'translate('+(0)+','+(10*hPyrPer)+')');
 		}
 
 		function pyramide(d, jahr, renderAusl,spiegel) {
@@ -585,7 +660,7 @@
 	    var xAxisF = d3.axisBottom()
 				.scale(xScale)
 				.ticks(4)
-        .tickSize(hPyr+13-10*hPyrPer)
+        .tickSize(hPyr-10*hPyrPer+8)
         .tickFormat(chFormat.format(','));
 
 			xScaleM = d3.scaleLinear()
@@ -596,7 +671,7 @@
 				.scale(xScaleM)
 				.ticks(4)
         .tickFormat(chFormat.format(','))
-        .tickSize(hPyr+13-10*hPyrPer);
+        .tickSize(hPyr-10*hPyrPer+8);
 
 		  d3.select('#xAxisF')
 				.transition()
@@ -626,9 +701,6 @@
 				trans = 0.9;
 			}
 
-
-
-
 			var rectFAusl = pyrGr.selectAll('rect.frauenAusl')
 				.data(pyrData);
 			rectFAusl.enter()
@@ -649,7 +721,6 @@
 			
 			var rectFCH = pyrGr.selectAll('rect.frauenCH')
 				.data(pyrData);
-
 			rectFCH.enter()
 				.append('rect')
 				.attr('class', 'frauenCH')
@@ -658,8 +729,6 @@
 				.attr('width', function(d) { return xScale(objAcc(d, 'fCH')) ;})
 				.style('fill-opacity', trans)
 				.attr('height', hPyrPer);
-
-
 			rectFCH
 				.transition()
 				.duration(200)
@@ -667,7 +736,6 @@
 				.attr('x', function(d) { return xScale(auslF(d));})//objAcc(d, 'mAusl') ;})
 				.attr('width', function(d) { return xScale(objAcc(d, 'fCH')) ;})
 				.style('fill-opacity', trans);
-
 			rectFCH.exit().remove()
 
 			var rectMAusl = pyrGr.selectAll('rect.maennerAusl')
@@ -681,7 +749,6 @@
 				.attr('height', hPyrPer)
 				.style('fill-opacity', trans)
 				.attr('transform', 'scale('+spiegel+',1)');
-			
 			rectMAusl
 				.transition()
 				.duration(200)
@@ -691,12 +758,10 @@
 				.attr('width', function(d) { return xScale(auslM(d)) ;})
 				.style('fill-opacity', trans)
 				.attr('transform', 'scale('+spiegel+',1)');
-			
 			rectMAusl.exit().remove()
-			
+		
 			var rectMCH = pyrGr.selectAll('rect.maennerCH')
 				.data(pyrData);
-			
 			rectMCH.enter()
 				.append('rect')
 				.attr('class', 'maennerCH')
@@ -726,13 +791,18 @@
 				.style('fill-opacity', 0.3)
 				.style('stroke-opacity', 0.3);
 
-			var mouseOverRW = 100/scaleF,
-				mouseOverRH = 40;
+			var dataJahr = dataFilter(thisData.properties.summen, 'key', jahrSel)[0];
+
+			//Rechteck Auswählen, welches zur Gemeinde passt:
+			d3.select('#legGr').selectAll('rect')
+
+			var mouseOverRW = 180/scaleF,
+				mouseOverRH = 108;
 			//Position Tooltip
 			var xPos = bbox.x+bbox.width/2,
 				yPos = bbox.y+bbox.height/2;
 			//Korrektur, damit tooltip nicht über den Rand hinaus geht:
-			if (xPos>200) {
+			if (xPos>0) {
 				xPos = bbox.x+bbox.width/2-mouseOverRW
 			}
 			if (yPos>80) {
@@ -762,7 +832,6 @@
 				.attr('fill-opacity', 0.8)
 				.style('stroke', 'dimgrey');
 
-
 			mouseOverT.append('path')
 				.attr("class", 'mouse')
 				.attr("d", that.attr('d'))
@@ -774,7 +843,16 @@
 				.attr('y', 16/scaleF)
 				.style('font-size', 12/scaleF+'px')
 				.style('font-weight', 'bold')
-				.text(thisData.properties.region__16);
+				.text(thisData.properties.region__16)
+				.style('font-family', 'Helvetica');
+
+			mouseOverT.append('text')
+				.attr('x', 2/scaleF)
+				.attr('y', 16/scaleF)
+				.attr('dy', '1.4em')
+				.style('font-size', 12/scaleF+'px')
+				.text('Anteil 20- bis 64-Jährige: ' +Math.round(dataJahr.value.arbJahr/dataJahr.value.totJahr*100)+'%')
+				.style('font-family', 'Helvetica');
 		}	
 
 		function mouseOut() {
@@ -786,6 +864,137 @@
 				.style('fill-opacity', 0.8)
 				.style('stroke-opacity', 1);
 		}
+
+		//////////////////////////////
+		//
+		//	Export 
+		//
+		d3.select('#expData').on('click', function(){ 
+			// var jsonBlob = new Blob([d3.select], { type : "text/plain", endings: "transparent"});
+			// saveAs(oMyBlob, "text.txt");
+			window.open('http://www.web.statistik.zh.ch/cms_vis/2017_MM_alterspyramide/data/STAT_ZH2017_bevoelkerung_regionen_prognose.csv')
+
+		})
+
+		d3.select('#expBild').on('click', function(){
+			var svgString = getSVGString(svg.node());
+			svgString2Image( svgString, 2*w, 2*h, 'png', save ); // passes Blob and filesize String to the callback
+
+			function save( dataBlob, filesize ){
+				saveAs( dataBlob, 'StatistischesAmtKtZuerich_Alterspyramide.png' ); // FileSaver.js function
+			}
+		});
+
+		function getSVGString( svgNode ) {
+			svgNode.setAttribute('xlink', 'http://www.w3.org/1999/xlink');
+			var cssStyleText = getCSSStyles( svgNode );
+			appendCSS( cssStyleText, svgNode );
+
+			var serializer = new XMLSerializer();
+			var svgString = serializer.serializeToString(svgNode);
+			svgString = svgString.replace(/(\w+)?:?xlink=/g, 'xmlns:xlink='); // Fix root xlink without namespace
+			svgString = svgString.replace(/NS\d+:href/g, 'xlink:href'); // Safari NS namespace fix
+
+			return svgString;
+
+			function getCSSStyles( parentElement ) {
+				var selectorTextArr = [];
+
+				// Add Parent element Id and Classes to the list
+				selectorTextArr.push( '#'+parentElement.id );
+				for (var c = 0; c < parentElement.classList.length; c++)
+						if ( !contains('.'+parentElement.classList[c], selectorTextArr) )
+							selectorTextArr.push( '.'+parentElement.classList[c] );
+
+				// Add Children element Ids and Classes to the list
+				var nodes = parentElement.getElementsByTagName("*");
+				for (var i = 0; i < nodes.length; i++) {
+					var id = nodes[i].id;
+					if ( !contains('#'+id, selectorTextArr) )
+						selectorTextArr.push( '#'+id );
+
+					var classes = nodes[i].classList;
+					for (var c = 0; c < classes.length; c++)
+						if ( !contains('.'+classes[c], selectorTextArr) )
+							selectorTextArr.push( '.'+classes[c] );
+				}
+
+				// Extract CSS Rules
+				var extractedCSSText = "";
+				for (var i = 0; i < document.styleSheets.length; i++) {
+					var s = document.styleSheets[i];
+					
+					try {
+					    if(!s.cssRules) continue;
+					} catch( e ) {
+				    		if(e.name !== 'SecurityError') throw e; // for Firefox
+				    		continue;
+				    	}
+
+					var cssRules = s.cssRules;
+					for (var r = 0; r < cssRules.length; r++) {
+						if ( contains( cssRules[r].selectorText, selectorTextArr ) )
+							extractedCSSText += cssRules[r].cssText;
+					}
+				}
+				
+
+				return extractedCSSText;
+
+				function contains(str,arr) {
+					return arr.indexOf( str ) === -1 ? false : true;
+				}
+
+			}
+
+			function appendCSS( cssText, element ) {
+				var styleElement = document.createElement("style");
+				styleElement.setAttribute("type","text/css"); 
+				styleElement.innerHTML = cssText;
+				var refNode = element.hasChildNodes() ? element.children[0] : null;
+				element.insertBefore( styleElement, refNode );
+			}
+		}	
+		function svgString2Image(svgString, width, height, format, callback ) {
+			console.log(svgString);
+			var format = format ? format : 'png';
+
+			var imgsrc = 'data:image/svg+xml;base64,'+ btoa( unescape( encodeURIComponent( svgString ) ) ); // Convert SVG string to data URL
+
+			var canvas = document.createElement("canvas");
+			var context = canvas.getContext("2d");
+
+			canvas.width = width;
+			canvas.height = height;
+
+			var image = new Image();
+			image.onload = function() {
+				context.clearRect ( 0, 0, width, height );
+				context.drawImage(image, 0, 0, width, height);
+
+				canvas.toBlob( function(blob) {
+					var filesize = Math.round( blob.length/1024 ) + ' KB';
+					if ( callback ) callback( blob, filesize );
+				});
+
+				
+			};
+
+			image.src = imgsrc;
+		}
+
+		function getKey(e) {
+			if (e.keyCode==80) {
+				var svgString = getSVGString(svg.node());
+				svgString2Image( svgString, 2*w, 2*h, 'png', save ); // passes Blob and filesize String to the callback
+
+				function save( dataBlob, filesize ){
+					saveAs( dataBlob, 'StatistischesAmtKtZuerich_Alterspyramide.png' ); // FileSaver.js function
+				}
+			}
+		}
+
+		document.onkeyup = getKey;
 
 	};
 }());
